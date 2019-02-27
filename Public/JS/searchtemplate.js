b60d9fb84723d2a6862c8d6c9985463a4f432bf4
Barry Scott Rollan <b.scott.rollan@gmail.com>
2019-02-27 14:13:02 -0500

    get master back in moreAPI

--- Public/JS/searchtemplate.js
+++ Public/JS/searchtemplate.js
@@ -7,108 +7,106 @@ $(function () {
         newsSearch(userInput);
         $(".searchBtn").val();
     };
-    
-    
+
+
     const newsSearch = function () {
         const queryURL = 'https://newsapi.org/v2/top-headlines?' +
             'country=us&' +
             'apiKey=4a91afd2bdda4b18be76a2f996628566';
-    
+
         $.ajax({
             url: queryURL,
             method: 'GET'
         }).then(function (response) {
             console.log(response.json());
-    
+
         })
     };
-    
+
     const parseAccessToken = function () {
         const index1 = window.location.hash.indexOf("=") + 1;
         const index2 = window.location.hash.indexOf("&");
         const accessToken = window.location.hash.substring(index1, index2);
         return accessToken;
     }
-    
+
     searchNewzy();
     // $(".searchBtn").on('click', searchNewzy);
-    
+
 });
 const sourceArray = []; //a list of verified sources to be used for creating custom buttons
 const articleList = [];
 const keyword = '"' + $('.searchBtn').val().trim() + '"';
 const sortByPopularity = 'sortBy=popularity';
-const  apiKey = 'apiKey=4a91afd2bdda4b18be76a2f996628566';
-const URL = `https://newsapi.org/v2/everything?`; 
+const apiKey = 'apiKey=4a91afd2bdda4b18be76a2f996628566';
+const URL = `https://newsapi.org/v2/everything?`;
 
 //compile an array of valid news sources (english)
-let sourceList = [];     
-let sourceCheck = '';                                         const sourcesURL = `https://newsapi.org/v2/sources?language=en&${apiKey}`; 
+let sourceList = [];
+let sourceCheck = '';
+const sourcesURL = `https://newsapi.org/v2/sources?language=en&${apiKey}`;
 
-$.ajax({j
-    url: sourcesURL,      
+$.ajax({
+    
+    url: sourcesURL,
     method: 'Get'
-}).then(function (resSources) { 
-    for(i=0; i<resSources.length; i++){
+}).then(function (resSources) {
+    for (i = 0; i < resSources.length; i++) {
         sourceCheck = resSources[i].id;
         sourceList.push(sourceCheck);
-    }                                  
+    }
     return sourceList;
 });
-////////////////////////////////////////////
 //Receiving Input to create Custom Buttons//
-////////////////////////////////////////////
-const addButton = function(event){
+const addButton = function (event) {
     event.preventDefault();
-    const sourceInput = $('#sourceInput').val().trim();
-    if(sourceList.includes(sourceInput)){//NEED A WAY OF GIVING OPTION BETWEEN SIMILAR i.e. abc / abc australia
-        
+    const sourceInput = $('#sourceInput').toLowerCase().substring(0, 3);
+    if (sourceList.includes(sourceInput)) {//NEED A WAY OF GIVING OPTION BETWEEN SIMILAR i.e. abc / abc australia
+
         sourceArray.push(sourceInput);
         render();
     }
-    else{
+    else {
         alert(`${sourceInput} is not a valid news source`);
     }
     $('#sourceInput').val('');
 };
-//////////////////////////////////////////
+
 //keyword search with default 20 results//
-/////////////////////////////////////////
-const genericKeyword = function(){
+const genericKeyword = function () {
     $.ajax({
         url: `${URL}q=${keyword}&${apiKey}`,
-        method: 'Get'                                            
+        method: 'Get'
     }).then(function (results) {
-        for(i=0; i<results.length; i++){
+        for (i = 0; i < results.length; i++) {
             articleList.push(results[i]);
-        }                         
+        }
         return articleList;
     });
-    console.log(articleList);  
+    console.log(articleList);
 };
-////////////////////////////////////////////////////////////
+
 //keyword search, get 3 separate sources from most-popular//
-////////////////////////////////////////////////////////////
-const threeSources = function(keyword){
+const threeSources = function (keyword) {
     $.ajax({
         url: `${URL}q=${keyword}&language=en&${sortByPopularity}&${apiKey}`,
-        method: 'Get'                                            
+        method: 'Get'
     }).then(function (results) {
         articleList.push(results[0]); //accepts first article
         sourceA = articleList[0].name;//assigns first article's name 
         sourceB = 'applesandoranges';
-        for(i=1; i<results.length; i++){
-            if(articleList.length > 2){ //break loop when articleList has 3 articles
+        for (i = 1; i < results.length; i++) {
+            if (articleList.length > 2) { //break loop when articleList has 3 articles
                 break;
             }
-            if(results[i].name !== sourceA && results[i].name !== sourceB){
-            articleList.push(results[i]);
+            if (results[i].name !== sourceA && results[i].name !== sourceB) {
+                articleList.push(results[i]);
             }
-            if(sourceB = 'applesandoranges'){
-                sourceB=results[i].name
+            if (sourceB == 'applesandoranges') {
+                sourceB = results[i].name
             }
-        }                         
+        }
         return articleList;
     });
-    console.log(articleList);  
+    console.log(articleList);
 };
