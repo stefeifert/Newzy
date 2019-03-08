1ce19fa6d6630470c7b5f85669c4d3d35b5e8e75
JTMERCH1 <falarirn@gmail.com>
2019-03-08 13:41:11 -0500

    committing changes

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,7 +1,28 @@
 import React from 'react';
 
+function toggleSidebar (){
+  document.getElementById("sidebar").classList.toggle('active');
+}
+
 const HomePage = () => (
+  <div>
   <div>Home</div>
+
+        <div id = "sidebar">
+        <div className="toggle-btn" onclick={toggleSidebar}>
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+        <ul>
+            <li>Home</li>
+            <li>My Profile</li>
+            <li>My Saved Articles</li>
+            <li>Log Out</li>
+        </ul>
+    </div>
+    
+  </div>
 
 )
 
