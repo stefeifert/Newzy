e1aaab9fdd8257d0932376514b43f12557d0c6f9
Barry Scott Rollan <b.scott.rollan@gmail.com>
2019-03-11 18:36:43 -0400

    adds keyword search functionality

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import '../App.css';
 import axios from 'axios'
-import NewzyCalls from './NewzyCalls';
+import SourceButtons from './SourceButtons';
 import KeywordSearch from './KeywordSearch';
 import Dashboard from "./dashboard/Dashboard";
 
@@ -16,95 +16,101 @@ class HomePage extends Component {
   state = {
     sources: [
       {
-        id:"cnn", 
+        id: "cnn",
         name: "CNN"
       },
       {
         id: "bbc-news",
         name: "BBC News"
-       },
-       {
+      },
+      {
         id: "fox-news",
         name: "FOX News"
-       } 
+      }
     ], //this array wil be used to compile buttons
-    newSource: {id: '', name: ''},
+    newSource: { id: '', name: '' },
     sourceList: [], //this will be populated with verified news sources
     sourceIds: [],
     isValidSource: false,
     keywordSearch: '',
     searchResults: []
-}
-
-createSourceList = () => {
-  axios.get('https://newsapi.org/v2/sources?language=en&apiKey=4a91afd2bdda4b18be76a2f996628566')
-  .then((result) => {
-    this.setState({sourceList: result.data});
-    console.log(this.state.sourceList);
-  })
-}
-searchResults = (event) => {
-  axios.get(`https://newsapi.org/v2/everything?q="${this.state.keywordSearch}"&apiKey=4a91afd2bdda4b18be76a2f996628566`)
-  .then((result) => {
-    this.setState({searchResults:[...result]});
-    this.setState({keywordSearch: ''});
-  })
-}
-searchChangeHandler = (event) => {
-  event.preventDefault();
-  const keywordSearch = event.target.value;
-  this.setState({keywordSearch: keywordSearch});
-}
-
-searchClickHandler = (event) => {
-  event.preventDefault();
-  this.searchResults();
-}
-
-sourcesChangeHandler = (event) => {
-  event.preventDefault();
-  const inputSource = {
-    id: event.target.value.replace(/\s+/g, "-").toLowerCase(),//changes "Al Jazeera" to "al-jazeera"
-    name: event.target.value
-  }
-  this.setState({newSource: inputSource});
-}
-
-sourcesClickHandler = (event) => {
-      event.preventDefault();
-      this.setState({sources:[...this.state.sources, this.state.newSource]});
-      this.setState({newSource: ''});
-      console.log("sources: " + this.state.sources);
   }
 
-  componentDidMount(){
+  createSourceList = () => {
+    axios.get('https://newsapi.org/v2/sources?language=en&apiKey=4a91afd2bdda4b18be76a2f996628566')
+      .then((result) => {
+        this.setState({ sourceList: result.data });
+        console.log(this.state.sourceList);
+      })
+  }
+  searchResults = (event) => {
+    axios.get(`https://newsapi.org/v2/everything?q="${this.state.keywordSearch}"&apiKey=4a91afd2bdda4b18be76a2f996628566`)
+      .then((result) => {
+        this.setState({ searchResults: result.data.articles });
+        console.log(this.state.searchResults);
+      })
+  }
+  searchChangeHandler = (event) => {
+    event.preventDefault();
+    const keywordSearch = event.target.value;
+    this.setState({ keywordSearch: keywordSearch });
+  }
+
+  searchClickHandler = (event) => {
+    event.preventDefault();
+    this.searchResults();
+  }
+
+  sourcesChangeHandler = (event) => {
+    event.preventDefault();
+    const inputSource = {
+      id: event.target.value.replace(/\s+/g, "-").toLowerCase(),//changes "Al Jazeera" to "al-jazeera"
+      name: event.target.value
+    }
+    this.setState({ newSource: inputSource });
+  }
+
+  sourcesClickHandler = (event) => {
+    event.preventDefault();
+    this.setState({ sources: [...this.state.sources, this.state.newSource] });
+    this.setState({ newSource: '' });
+    console.log("sources: " + this.state.sources);
+  }
+
+  componentDidMount() {
     this.createSourceList();
   }
 
   render() {
-    const currentSources = this.state.sources;
+    const currentSources = this.state.sources;  //button array of news sources
     const sourcesBtn = currentSources.map(
       source => {
-        return(
+        return (
           <button key={source.id}>{source.name}</button>
         )
       }
     )
     return (
-      <div className="App">
+      <div className="HomePage">
         <Dashboard />
-        <NewzyCalls 
-
-        sourcesChangeHandler={this.sourcesChangeHandler}
-        btnRow={this.state.createButtons}
-        sourcesClickHandler={this.sourcesClickHandler}
+        <SourceButtons
+          sourcesChangeHandler={this.sourcesChangeHandler}
+          btnRow={this.state.createButtons}
+          sourcesClickHandler={this.sourcesClickHandler}
         />
         <span>{sourcesBtn}</span>
         <KeywordSearch
-        searchChangeHandler={this.searchChangeHandler}
-        searchClickHandler={this.searchClickHandler}
-        searchResults={this.state.searchResults}
+          searchChangeHandler={this.searchChangeHandler}
+          searchClickHandler={this.searchClickHandler}
+          searchResults={this.state.searchResults}
         />
+        <ul>
+        {this.state.searchResults.map(d => <li key={d.publishedAt}>
+        {d.title} / {d.source.name} / {d.author} /<a href={d.url}>{d.url} </a>
+        <hr></hr>
+        </li>)}
+        </ul>
+
       </div>
     );
   }
--- client/src/components/KeywordSearch.js
+++ client/src/components/KeywordSearch.js
@@ -1,14 +1,14 @@
 import React from 'react';
  
-const KeywordSearch = props => (
+const KeywordSearch = (props) => (
     <div>
-        <span><input 
-        onChange={props.searchChangeHandler}
-        value={props.value}/>
-        <button onClick={props.searchClickHandler}>keyword search             
-        </button>
-        </span>
-        {props.searchResults}
+        <div>
+            <input 
+            onChange={props.searchChangeHandler}
+            value={props.value}/>
+            <button onClick={props.searchClickHandler}>keyword search             
+            </button>
+        </div>
     </div>
 )
 
