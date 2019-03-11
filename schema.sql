5297065f4daada8f0020124be505b86712ca2f25
Nihit Tiwari <nihitter05@gmail.com>
2019-03-11 11:46:23 -0400

    changed the db to Mongo DB. Added in the Passport authentication along with some UI changes

--- schema.sql
+++
@@ -1,4 +0,0 @@
--- Drops the Article DB if it exists currently --
-DROP DATABASE IF EXISTS Newzy_db;
--- Creates the "article" database --
-CREATE DATABASE Newzy_db;
--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const path = require('path');
-const db = require('./models')
+const express = require("express");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const users = require("./Routes/users");
+const apiRoutes = require("./Routes/apiRoutes");
 
+//Initiate our app & PORT
 const app = express();
-
 const PORT = process.env.PORT || 3001;
-
-
 
 app.use(express.urlencoded({ success: true }));
 app.use(express.json());
@@ -15,12 +15,27 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-require('./routes/apiRoutes.js')(app);
-// require('./routes/html-routes.js')(app);
 
-db.sequelize.sync({ force: true }).then(function() {
-    app.listen(PORT, function() {
-      console.log('App listening on PORT ' + PORT);
-    });
-  });
-  
+// Bodyparser middleware
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+// DB Config
+const dbURI = require("./passport-config/key").mongoURI;
+// Connect to MongoDB
+mongoose
+  .connect(dbURI, { useNewUrlParser: true })
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch(err => console.log(err));
+
+
+// Passport middleware
+app.use(passport.initialize());
+// Passport config
+require("./passport-config/passport")(passport);
+// Routes
+app.use("/api/users", users);
+app.use(apiRoutes);
+
+app.listen(PORT, () => {
+	console.log(`App listening on PORT: ${PORT}`)
+})
