0aebfcb7a84e50c69f3d271f59e347673466f535
JTMERCH1 <falarirn@gmail.com>
2019-03-12 11:50:20 -0400

    more changes

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,49 +1,50 @@
 import React, { Component } from "react";
 import {Link} from "react-router-dom";
+import background from './newsBackground.jpg'
 
 class Landing extends Component {
   render() {
     return (
-      <div style={{ height: "75vh" }} className="container valign-wrapper">
-        <div className="row">
-          <div className="col s12 center-align">
-            <h4>
-              <b>Build</b> a login/auth app with the{" "}
-              <span style={{ fontFamily: "monospace" }}>MERN</span> stack from
-              scratch
-            </h4>
-            <p className="flow-text grey-text text-darken-1">
-              Create a (minimal) full-stack app with user authentication via
-              passport and JWTs
-            </p>
-           <br />
-            <Link
-              to="/register"
-              style={{
-                width: "150px",
-                borderRadius: "3px",
-                letterSpacing: "1.5px"
-              }}
-              className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-            >
-              Register
-            </Link>
-            <Link
-            to="/login"
-              style={{
-                marginLeft: "2rem",
-                width: "150px",
-                borderRadius: "3px",
-                letterSpacing: "1.5px"
-              }}
-              className="btn btn-large waves-effect white hoverable black-text"
-            >
-              Log In
-            </Link>
+      console.log(background),
+      <img src={background} alt="newsBackground"></img>
+        <div style={{ height: "75vh" }} className="container valign-wrapper">
+          <div className="row">
+            <div className="col s12 center-align">
+              <h4>
+                <span style={{ fontFamily: "Arial Helvetica", fontSize: 50 }}>The one place you need to go for all your news needs!</span>
+              </h4>
+              <p className="flow-text grey-text text-darken-1">
+
+              </p>
+            <br />
+              <Link
+                to="/register"
+                style={{
+                  width: "150px",
+                  borderRadius: "3px",
+                  letterSpacing: "1.5px"
+                }}
+                className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+              >
+                Register
+              </Link>
+              <Link
+              to="/login"
+                style={{
+                  marginLeft: "2rem",
+                  width: "150px",
+                  borderRadius: "3px",
+                  letterSpacing: "1.5px"
+                }}
+                className="btn btn-large waves-effect white hoverable black-text"
+              >
+                Log In
+              </Link>
+            </div>
           </div>
         </div>
-      </div>
-    );
-  }
+      );
+    }
 }
+ 
 export default Landing;
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -15,7 +15,7 @@ class Navbar extends Component {
               }}
               className="col s5 brand-logo center black-text"
             >
-              <i className="material-icons">code</i>
+              <i class="fas fa-newspaper"></i>
               Newzy
             </Link>
           </div>
