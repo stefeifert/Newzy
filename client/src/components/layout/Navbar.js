b972b4d8ac8002b8afb982d033156bbc3de79555
JTMERCH1 <falarirn@gmail.com>
2019-03-12 10:56:38 -0400

    changes

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
+
 class Navbar extends Component {
   render() {
     return (
@@ -14,8 +15,8 @@ class Navbar extends Component {
               }}
               className="col s5 brand-logo center black-text"
             >
-              <i className="material-icons">code</i>
-              MERN
+              <i className="fas fa-newspaper"></i>
+              Newzy
             </Link>
           </div>
         </nav>
